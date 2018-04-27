2147c0f5f928e49a5e01e93235631a5daa133ce3
Trontor <Trontor@users.noreply.github.com>
2018-04-27 15:58:29 +1000

    Folder Organisation

--- program.c
+++ program.c
@@ -3,13 +3,6 @@
 #define MAXVAL 1000
 
 int main(int argc, char* argv[]) {
-	printf("Enter values, ^D to terminate:\n");
-	int A[MAXVAL], next, counter = 0;
-	while (scanf("%d", &next) == 1) {
-		A[counter++] = next;
-	}
-	char c = getchar();
-	printf("%d", c);
-	printf("\nFind the k'th smallest element  (k < %d):", counter + 1);
-
+	int nprimes = 1, n1 = 10, n2 = 12;
+	printf("%f%%", 1.0*nprimes/(n2-n1)*100);
 }
